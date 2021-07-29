955e6a9c768f26d1f66aea8b5c4821c68347fae6
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-07-29 13:01:02 +0200

    removed shit and added db exports

--- api/models.py
+++ api/models.py
@@ -1,38 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
  
 db = SQLAlchemy()
- 
-""" class model_props(db.Model):
-    __tablename__ = 'model1'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    startsum_low = db.Column(db.Integer)
-    startsum_high = db.Column(db.Integer)
-    age_low = db.Column(db.Integer)
-    age_high = db.Column(db.Integer)
-    distance = db.Column(db.Integer)
-    starttype = db.Column(db.Integer)
-    gender = db.Column(db.Integer)
-    applicants = db.Column(db.Integer)
-    addition = db.Column(db.Integer)
-    first_price = db.Column(db.Integer)
-    good_prop = db.Column(db.Integer)
- 
-    def __init__(self, startsum_low, startsum_high, age_low, age_high, distance, starttype, gender, applicants, addition, first_price, good_prop):
-        self.startsum_low = startsum_low
-        self.startsum_high = startsum_high
-        self.age_low = age_low
-        self.age_high = age_high
-        self.distance = distance
-        self.starttype = starttype
-        self.gender = gender
-        self.applicants = applicants
-        self.addition = addition
-        self.first_price = first_price
-        self.good_prop = good_prop
- 
-    def __repr__(self):
-        return f"{self.startsum_low}:{self.startsum_high}:{self.age_low}:{self.age_high}:{self.distance}:{self.starttype}:{self.gender}:{self.applicants}:{self.addition}:{self.first_price}:{self.good_prop}" """
-
+#Prop database
 class model_props(db.Model):
     __tablename__ = 'model2'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -68,6 +37,7 @@ class model_props(db.Model):
     def __repr__(self):
         return f"{self.startsum_low}:{self.startsum_high}:{self.age_low}:{self.age_high}:{self.distance}:{self.starttype}:{self.gender}:{self.applicants}:{self.addition}:{self.first_price}:{self.good_prop}:{self.starting_total}:{self.starting_women}"
 
+#Horses in vicinity database
 class model_horse(db.Model):
     __tablename__ = 'horse'
     id = db.Column(db.Integer, primary_key=True)
